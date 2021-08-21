db11cd45fd8b065bd15fc976fdf8afa8d7f367e7
srijakancharla14 <87753850+srijakancharla14@users.noreply.github.com>
2021-08-21 13:09:24 +0530

    this is the second version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 const name = "srija";
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
 ReactDOM.render(
   <div>
     <h1>hi {name}</h1>
-    <h1>lucky number yeah {Math.floor(Math.random() * 10)}</h1>
+    <p>hey hi how's life. Today is {currentDate + "" + currentYear} </p>
   </div>,
   document.getElementById("root")
 );
